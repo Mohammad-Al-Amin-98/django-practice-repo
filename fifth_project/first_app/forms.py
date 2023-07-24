437b52399a909106635c80ed93aa2250c52c8ab0
TheWizardPro <wannabeprogrammer007@yandex.com>
2023-07-24 21:15:44 +0600

    Second Commit

--- fifth_project/first_app/forms.py
+++ fifth_project/first_app/forms.py
@@ -2,4 +2,30 @@ from django import forms
 
 class contactForm(forms.Form):
     name = forms.CharField(label="User Name")
+    #file = forms.FileField()
     email = forms.EmailField(label="User Email")
+    age = forms.IntegerField()
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    check = forms.BooleanField()
+    birthday = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    appointment = forms.DateTimeField(widget=forms.DateInput(attrs={'type':'datetime-local'}))
+    CHOICES = [('S','Small'),('M','Medium'),('L','Large')]
+    size = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    MEAL = [('P','Pepperoni'),('M','Mashroom'),('B','Beef')]
+    pizza = forms.MultipleChoiceField(choices=MEAL, widget=forms.CheckboxSelectMultiple)
+
+class StudentData(forms.Form):
+    name = forms.CharField(widget=forms.TextInput)
+    email = forms.CharField(widget=forms.EmailInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        valname = self.cleaned_data['name']
+        valemail = self.cleaned_data['email']
+
+        if len(valname) < 10:
+            raise forms.ValidationError('Enter a name at least with 10 characters!')
+
+        if '.com' not in valemail:
+            raise forms.ValidationError('Your email must contain .com!')
--- fifth_project/first_app/views.py
+++ fifth_project/first_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import contactForm
+from .forms import contactForm, StudentData
 
 # Create your views here.
 def home(request):
@@ -18,7 +18,24 @@ def submit_form(request):
     return render(request,'./first_app/form.html')
 
 def DjangoForm(request):
-    form = contactForm(request.POST)
-    if form.is_valid():
-        print(form.cleaned_data)
-    return render(request,'./first_app/django_form.html',{'form':form})
+    if request.method == 'POST':
+        form = contactForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+            file = form.cleaned_data['file']
+            with open('./first_app/upload/'+file.name, 'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+        return render(request,'./first_app/django_form.html',{'form':form})
+    else:
+        form = contactForm()
+        return render(request, './first_app/django_form.html', {'form':form})
+
+def studentData(request):
+    if request.method == 'POST':
+        form = StudentData(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = StudentData()
+    return render(request, './first_app/django_form.html', {'form':form})
